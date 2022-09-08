1918368ea8b5561a86bffb7b8f68882bd2a585f0
juliocromerosooft <jromero@sooft.com.ar>
2022-09-08 16:22:03 -0300

    linguo de 0

---
+++ react-linguoo-ex-fakeflix-new/src/App.js
@@ -0,0 +1,162 @@
+import { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { Route, Switch, Redirect, useLocation } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar/Navbar"
+import Homepage from "./pages/Homepage/Homepage"
+import Movies from "./pages/Movies/Movies"
+import TVSeries from './pages/TVSeries/TVSeries';
+import Popular from "./pages/Popular/Popular";
+import MyList from './pages/MyList/MyList';
+import Auth from "./pages/Auth/Auth";
+import Search from "./pages/Search/Search";
+import Category from "./pages/Category/Category";
+import DetailModal from "./components/DetailModal/DetailModal";
+// import SplashAnimation from "./components/SplashAnimation/SplashAnimation";
+import PlayAnimation from "./components/PlayAnimation/PlayAnimation";
+import { selectCurrentUser } from './redux/auth/auth.selectors';
+import { selectSearchResults } from "./redux/search/search.selectors";
+import { checkUserSession } from "./redux/auth/auth.actions";
+import LinguooAnimationPlay from "./components/PlayAnimation/LinguooAnimationPlay";
+import  PlayListContent  from "./pages/PlayListContent/PlayListContent";
+// import { AudioPlayer } from './components/AudioPlayer/AudioPlayer';
+// import { fetchFavouritesAsync } from './redux/favourites/favourites.actions';
+import requests from './requests';
+import { setDefaultAudioListValuesAsync } from './redux/audioplaying/audioplaying.actions';
+import { fetchNarratorsAsync } from './redux/narrators/narrators.actions';
+import AudioPlayer from "./components/AudioPlayer/AudioPlayer";
+// import { getLocalStorageCurrentUser } from './shared/localStorage'
+
+const App = () => {
+
+    const currentUser = useSelector(selectCurrentUser);
+    // const currentUser = getLocalStorageCurrentUser();
+    const searchResults = useSelector(selectSearchResults);
+    const dispatch = useDispatch();
+    const location = useLocation();
+    // dispatch(fetchFavouritesAsync);
+    if(currentUser){
+        const urlDefaultAudioValues = requests.fetchTrendingMovies;
+        dispatch(setDefaultAudioListValuesAsync(urlDefaultAudioValues));
+        const urlNarrators = requests.fetchNarrators;
+        dispatch(fetchNarratorsAsync(urlNarrators));
+    }
+
+    useEffect(() => {
+        dispatch(checkUserSession());
+    }, [dispatch])
+
+    return (
+        <div className="App">
+            {currentUser && (
+                <>
+                    <Navbar />
+                    <DetailModal />
+                </>
+            )}
+            <AnimatePresence exitBeforeEnter>
+                <Switch location={location} key={location.pathname}>
+                    <Route
+                        exact
+                        path="/"
+                    >
+                        <Redirect to="/login" />
+                    </Route>
+                    <Route
+                        path="/play"
+                        component={PlayAnimation}
+                    />
+                    <Route
+                        path="/linguooplay/:audioName"
+                        component={LinguooAnimationPlay}
+                    />
+                    <Route
+                        path="/search"
+                        render={() => currentUser
+                            ? (searchResults && <Search results={searchResults}/>)
+                            : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/browse"
+                        render={() => currentUser
+                            ? <Homepage />
+                            : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/browse/:categoryName"
+                        render={(props) => currentUser
+                            ? <Category {...props} />
+                            : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/playlistcontent"
+                        render={() => currentUser
+                            ? <PlayListContent />
+                            : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/tvseries"
+                        render={() => currentUser ? <TVSeries /> : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/tvseries/:categoryName"
+                        render={(props) => currentUser
+                            ? <Category {...props} />
+                            : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/movies"
+                        render={() => currentUser ? <Movies /> : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/movies/:categoryName"
+                        render={(props) => currentUser
+                            ? <Category {...props} />
+                            : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/popular"
+                        render={() => currentUser ? <Popular /> : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/popular/:categoryName"
+                        render={(props) => currentUser
+                            ? <Category {...props} />
+                            : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/mylist"
+                        render={() => currentUser ? <MyList /> : <Redirect to="/login" />}
+                    />
+                    <Route
+                        exact
+                        path="/login"
+                        render={() => currentUser ? <Redirect to="/browse"/> : <Auth />}
+                    />
+                    <Route path="*">
+                        <Redirect to="/" />
+                    </Route>
+                </Switch>
+                <div style={{ position: 'relative', marginTop: '6em'}}></div>
+            </AnimatePresence>
+            {currentUser && (
+                <>
+                    <AudioPlayer />
+                </>
+            )}
+
+        </div>
+    )
+}
+
+export default App;
